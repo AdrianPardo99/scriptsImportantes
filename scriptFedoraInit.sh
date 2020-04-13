82677b59ef85c91e285b09341c2c2c9451962ccf
Adrian González Pardo <gozapaadr@gmail.com>
2020-04-13 17:29:16 -0500

    Fedora install packages

--- scriptFedoraInit.sh
+++ scriptFedoraInit.sh
@@ -2,5 +2,5 @@
 echo "Script init fedora"
 sudo -l
 echo s | sudo dnf update
-echo s | sudo dnf install git neovim vim python3 python3-pip fish terminator openssh nmap @custom-enviroment @cinnamon-desktop-environment @container-management @libreoffice @admin-tools @authoring-and-publishing @books @c-development @design-suite @development-tools @electronic-lab @engineering-and-scientific @python-science @rpm-development-tools @sound-and-video @system-tools @window-managers
+echo s | sudo dnf install git neovim vim python3 python3-pip fish terminator openssh nmap @custom-environment @cinnamon-desktop-environment @container-management @libreoffice @admin-tools @authoring-and-publishing @books @c-development @design-suite @development-tools @electronic-lab @engineering-and-scientific @python-science @rpm-development-tools @sound-and-video @system-tools @window-managers
 echo "Thats all bye"
